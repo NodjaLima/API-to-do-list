28d29c4849838fd998a6f820b5c13881f1133b01
NodjaLima <nodjamplima@gmail.com>
2022-07-19 23:36:16 -0300

    criado rota /usuarios com verbo GET no arquivo app.js

--- src/app.js
+++ src/app.js
@@ -8,6 +8,10 @@ app.get('/usuarios', (rep, resp) => {
   console.log(`funcionando na porta ${porta}`)
 })
 
+app.get('/tarefas', (res, resp) => {
+  resp.send(`Rota ativada com GET e recurso Tarefas: valores de Tarefas devem ser retornados`)
+})
+
 app.listen(porta, () => {
   console.log(`Servidor funcionando na porta ${porta}`)
 })
