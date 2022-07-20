a29a3474804d26245da967fbaf509dadd23f9a93
NodjaLima <nodjamplima@gmail.com>
2022-07-20 10:43:32 -0300

    Insere dados mockados no bd.js

---
+++ src/utils/bd.js
@@ -0,0 +1,45 @@
+export class Database {
+  usuario = [
+    {
+      nome : "Mariana Lima",
+      email : "marianalima@meuemail.com",
+      senha : "senhadamariana"
+    },
+    {
+      nome : "Miguel Gall",
+      email : "miguelgall@meuemail.com",
+      senha : "senhadomiguel"
+    },
+    {
+      nome : "William Henrique",
+      email : "will.bolado@meuemail.com",
+      senha : "senhadowilliam"
+    },
+    {
+      nome : "Nodja Lima",
+      email : "nodjamoama@meuemail.com",
+      senha : "senhadamommydev"
+    }
+  ]
+  tarefa = [
+    {
+      titulo : "Brincar com o cachorro", 
+      descricao: "Levar para correr na praia no final da tarde", 
+      status : "Fazer", 
+      dataDeCriacao : "20/01/2022"
+    },
+    {
+      titulo : "Regar as plantas", 
+      descricao: "Com a mangueira que saí do chuveiro da garagem, regar as plantas", 
+      status : "Feito", 
+      dataDeCriacao : "20/01/2022"
+    },
+    {
+      titulo : "Descongelar feijão para o jantar", 
+      descricao: "Tirar o pote de fake sorvete do congelador antes de 16h da tarde", 
+      status : "Feito", 
+      dataDeCriacao : "19/01/2022"
+    },
+
+  ]
+}
